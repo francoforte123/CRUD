5d9bfb019fdd74813876a5457e5502d69a63d0a0
Emmanuele La Duca <laducaemanuele20@gmail.com>
2022-10-25 14:43:39 +0200

    resolve small problem

--- src/main/java/CRUD/Entities/Car.java
+++ src/main/java/CRUD/Entities/Car.java
@@ -15,4 +15,29 @@ public class Car {
     private String id;
     private String modelName;
     private String type;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
